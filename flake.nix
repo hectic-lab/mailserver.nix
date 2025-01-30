9861ee310cfd6bc1cb45320aa540260c171d4b29
George <rozov2004@list.ru>
2025-01-30 04:51:06 +0300

    refactor: got it working

---
+++ flake.nix
@@ -0,0 +1,141 @@
+{
+  description = "NixOS mail server flake";
+
+  inputs = {
+    nixos-mailserver = {
+      url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    y-util = {
+      url = "github:yukkop/y.util.nix";
+      inputs.nixpkgs.follows = "nixpkgs"; 
+    };
+  };
+
+  outputs = { self, nixpkgs, deploy-rs, y-util, nixos-mailserver }:
+    let
+      lib = nixpkgs.lib;
+
+      overlays = [ ];
+
+      forAllSystemsWithPkgs = y-util.lib.forAllSystemsWithPkgs overlays;
+    in
+    forAllSystemsWithPkgs ({ system, pkgs }:
+    {
+      packages.${system} = { };
+      devShells.${system}.default = pkgs.mkShell { };
+      nixosModules.${system} = {
+        mailserver = import ./mail.nix;
+      };
+  }) // {
+      # test on vm
+      # `nix run .#nixosConfigurations.accord.config.system.build.vm`
+      nixosConfigurations.accord =
+      let
+        system = "x86_64-linux";
+      in
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ( nixos-mailserver.nixosModules.mailserver )
+          ( import ./mail.nix )
+          ({ pkgs, modulesPath, ... }: {
+            imports = [
+            ];
+
+            environment.systemPackages = with pkgs; [ 
+              curl
+              neovim
+            ];
+
+            # Autologin root in the VM
+            services.getty.autologinUser = "root";
+
+            virtualisation.vmVariant = {
+              virtualisation.qemu.options = [
+                "-nographic" 
+                "-display" "curses"
+                "-append" "console=ttyS0"
+                "-serial" "mon:stdio"
+                "-vga" "qxl"
+              ];
+              virtualisation.forwardPorts = [
+                { from = "host"; host.port = 40500; guest.port = 22; }
+              ];
+            };
+
+            users.users.root.openssh.authorizedKeys.keys = [
+                ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILJHnBiiapioSPXJJhi/n+x0Gy1MDEoSh4s5zO+V3jJn yukkop@wsl''
+                ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKPbIJATVyAw7F7vBZbHkCODXFo5gvDyqhuU0gnNUNH snuff@jorge-desktop''
+            ];
+
+            services.openssh = {
+              enable = true;
+              settings = {
+                PasswordAuthentication = true;
+              };
+            };
+
+	    # hetzner settings
+            boot.loader.grub.device = "/dev/sda";
+            boot.initrd.availableKernelModules = [
+              "ata_piix"
+              "uhci_hcd"
+              "xen_blkfront"
+              "vmw_pvscsi"
+            ];
+
+            networking.firewall = {
+              enable = true;
+              allowedTCPPorts = [
+                80
+                443
+                53     # DNS
+              ];
+            };
+            services.mailserver.enable = true;
+            services.mailserver.domain = "hectic-lab.com";
+            services.mailserver.loginAccounts = {
+              "security" = {
+                hashedPassword = "$2b$05$Y48Db1heT.GRgQQKjS88w.ZW0f2hDrS3Z3MOeGbyRIsIrS1hOGqOe";
+              };
+              "yukkop" = {
+                hashedPassword = "$2b$05$RqChHPfJv9Q.eizwDbayk.u5UTmD3OSCfLHkBB2ixbd6VvRHfu8Xy";
+              };
+              "snuff" = {
+                hashedPassword = "$2b$05$fcnxDZwkv8rWCll4FPP8hupmDPDoty01FrarPSf1cIYtdbCO6XHSa";
+              };
+            };
+
+            boot.initrd.kernelModules = [ "nvme" ];
+            fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+            system.stateVersion = "24.05";
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      };
+
+      deploy.nodes.accord = let
+        inherit (self.nixosConfigurations.accord.config.nixpkgs) system;
+      in {
+        hostname = "accord";
+        fastConnection = true;
+        profilesOrder = [ "system" ];
+        sshOpts = [ ];
+        profiles."system" = {
+          sshUser = "root";
+          path = deploy-rs.lib.${system}.activate.nixos
+            self.nixosConfigurations.accord;
+          user = "root";
+        };
+      };
+  };
+}
+
---
+++ mail.nix
@@ -0,0 +1,44 @@
+{ lib, config, ... }: 
+let
+  cfg = config.services.mailserver;
+  transform_login_accounts = domain: input: builtins.listToAttrs (map (key: {
+      name = key + "@" + domain;
+      value = input.${key};
+    }) (builtins.attrNames input));
+in
+{
+  options = {
+      services.mailserver.enable = lib.mkEnableOption "Mail server";
+      services.mailserver.domain = lib.mkOption {
+        type = lib.types.str;
+        description = "The domain name of the mail server";
+      };
+      services.mailserver.loginAccounts = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          options = {
+            hashedPassword = lib.mkOption {
+              type = lib.types.str;
+            };
+          };
+        });
+
+        default = {};
+        description = "A list of all login accounts";
+      };
+  };
+  config = lib.mkIf cfg.enable {
+    mailserver = {
+      enable = true;
+      fqdn = "mail." + cfg.domain;
+      domains = [ cfg.domain ];
+      enableSubmissionSsl = false;
+
+      loginAccounts = transform_login_accounts cfg.domain cfg.loginAccounts;
+  
+      certificateScheme = "acme-nginx";
+    };
+    security.acme.acceptTerms = true;
+    security.acme.defaults.email = "security@" + cfg.domain;
+  };
+}
